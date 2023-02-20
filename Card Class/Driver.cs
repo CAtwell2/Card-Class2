20bdc66df66068e33bfeb0e4e86e7da73b5e5350
Caden <Caden@DESKTOP-FM9BGJ0>
2023-02-19 23:18:19 -0500

    Add project files.

---
+++ Card Class/Driver.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace Card_Class
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            Card defaultCard = new Card();
+            Console.WriteLine($"The default card is:{defaultCard.ToString()}");
+
+            int input = 0;
+            Console.Write("Please type an integer between 1 and 52. Type -1 to stop:");
+            input = int.Parse(Console.ReadLine());
+
+            while (input != -1)
+            {
+                Card card = new Card(input);
+                Console.WriteLine($"The card matching {input} is the {card.ToString()}");
+                Console.Write("Please type an integer between 1 and 52. Type -1 to stop:");
+                input = int.Parse(Console.ReadLine());
+            }
+
+            ShowDeck();
+        }
+
+
+        //loop to keep prompting the user unless they enter -1
+        private static void ShowDeck()
+        {
+            Console.WriteLine("\nAll possible cards in a deck:");
+            for (int i = 0; i < 52; i++)
+            {
+                Card card = new Card(i);
+                Console.WriteLine(card.ToString());
+            }
+        }
+    }
+}
---
+++ Card Class/Program.cs
@@ -0,0 +1,83 @@
+﻿
+
+using CardClass;
+
+namespace CardClass
+{
+    //enums for the face and suit of the card
+    public enum Face
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+}
+
+public enum Suit
+{
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades
+}
+
+public class Card
+{
+    private Face face;
+    private Suit suit;
+
+    //default constructor
+    public Card()
+    {
+        face = Face.Ace;
+        suit = Suit.Spades;
+    }
+    //copy constuctor
+    public Card(Card existingCard)
+    {
+        face = existingCard.face;
+        suit = existingCard.suit;
+    }
+    //parameterized constructor
+    public Card(int n)
+    {
+        face = (Face)Enum.GetValues(typeof(Face)).GetValue(n % 13);
+        suit = (Suit)Enum.GetValues(typeof(Suit)).GetValue(n % 4);
+    }
+    //getters and setters 
+    public Face GetFace()
+    {
+        return face;
+    }
+
+    public void SetFace(Face face)
+    {
+        this.face = face;
+    }
+
+    public Suit GetSuit()
+    {
+        return suit;
+    }
+
+    public void SetSuit(Suit suit)
+    {
+        this.suit = suit;
+    }
+
+    //to string method 
+    public override string ToString()
+    {
+        return $"The {face} of {suit}";
+    }
+}
